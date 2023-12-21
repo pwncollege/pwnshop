09a82dd46b90cad8a7efa092ba626d5af95ad5d2
Yan <yans@yancomm.net>
2023-12-21 12:27:37 -0700

    bump version and remove yaml reqs

--- setup.py
+++ setup.py
@@ -7,10 +7,10 @@ packages = find_packages(exclude=["example_module"]) + [ "pwnshop.challenges.bas
 
 setup(
     name="pwnshop",
-    version="1.0",
+    version="2.0",
     python_requires=">=3.8",
     packages=packages,
-    install_requires=["jinja2==3.0.3", "nbconvert==6.4.4", "asteval", "pyastyle", "pwntools", "ezmp", "pyyaml", "ruamel.yaml", "docker"],
+    install_requires=["jinja2==3.0.3", "nbconvert==6.4.4", "asteval", "pyastyle", "pwntools", "ezmp", "docker"],
     package_data={ dd.replace("/","."):cc for dd,cc in ( (d,[f for f in c if f.endswith(".c")]) for d,_,c in os.walk("pwnshop") ) if cc },
     description="A framework for generating CTF challenges for learning",
     url="https://github.com/pwncollege/pwnshop",
