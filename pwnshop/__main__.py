25880bf5e58928ec8ab861d6189eee6fb3fab745
Yan <yans@yancomm.net>
2024-10-28 16:20:51 -0700

    support specifying multiple challenges

--- pwnshop/__main__.py
+++ pwnshop/__main__.py
@@ -20,46 +20,51 @@ def challenge_class(challenge):
         assert 0 < level <= len(pwnshop.MODULE_LEVELS[module]), "Invalid level specified."
         return pwnshop.MODULE_LEVELS[module][level-1]
 
-def with_challenge(f):
+def with_challenges(f):
     @functools.wraps(f)
     def f_with_challenge(args):
-        challenge = challenge_class(challenge=args.challenge)(seed=args.seed, walkthrough=args.walkthrough)
-        return f(args, challenge)
+        challenges = [
+            challenge_class(challenge=c)(seed=args.seed, walkthrough=args.walkthrough)
+            for c in args.challenges
+        ]
+        return f(args, challenges)
     return f_with_challenge
 
-@with_challenge
-def handle_render(args, challenge):
-    src = challenge.generate_source()
-    if not args.lineno:
-        args.out.write(src+"\n")
-    else:
-        for i, line in enumerate(src.splitlines()):
-            args.out.write(f"{i + 1}\t{line}\n")
-    if os.path.isfile(args.out.name):
-        os.chmod(args.out.name, 0o644)
+@with_challenges
+def handle_render(args, challenges):
+    for challenge in challenges:
+        src = challenge.generate_source()
+        if not args.lineno:
+            args.out.write(src+"\n")
+        else:
+            for i, line in enumerate(src.splitlines()):
+                args.out.write(f"{i + 1}\t{line}\n")
+        if os.path.isfile(args.out.name):
+            os.chmod(args.out.name, 0o644)
 
-def handle_list(args):
+def handle_list(args): #pylint:disable=unused-argument
     for module,challenges in pwnshop.MODULE_LEVELS.items():
         for n,challenge in enumerate(challenges, start=1):
             print(f"{module}:{n} --- {challenge.__name__}")
 
-@with_challenge
-def handle_build(args, challenge):
-    binary, libs, pdb = challenge.build_binary()
-    args.out.buffer.write(binary)
-    if os.path.isfile(args.out.name):
-        os.chmod(args.out.name, 0o755)
+@with_challenges
+def handle_build(args, challenges):
+    for challenge in challenges:
+        binary, libs, pdb = challenge.build_binary()
+        args.out.buffer.write(binary)
+        if os.path.isfile(args.out.name):
+            os.chmod(args.out.name, 0o755)
 
-    if args.lpath and libs:
-        os.makedirs(args.lpath, exist_ok=True)
-        for lib_name, lib_bytes in libs:
-            lib_path = args.lpath + f'/{lib_name}'
-            with open(lib_path, 'wb+') as f:
-                f.write(lib_bytes)
-            os.chmod(lib_path, 0o755)
-    if pdb:
-        with open(f"{args.out.name.replace('.exe', '.pdb')}", 'wb') as f:
-            f.write(pdb)
+        if args.lpath and libs:
+            os.makedirs(args.lpath, exist_ok=True)
+            for lib_name, lib_bytes in libs:
+                lib_path = args.lpath + f'/{lib_name}'
+                with open(lib_path, 'wb+') as f:
+                    f.write(lib_bytes)
+                os.chmod(lib_path, 0o755)
+        if pdb:
+            with open(f"{args.out.name.replace('.exe', '.pdb')}", 'wb') as f:
+                f.write(pdb)
 
 def verify_challenge(challenge, debug=False, flag=None, strace=False):
     if debug:
@@ -74,9 +79,11 @@ def verify_challenge(challenge, debug=False, flag=None, strace=False):
     else:
         challenge.verify()
 
-@with_challenge
-def handle_verify(args, challenge):
-    return verify_challenge(challenge, debug=args.debug, flag=args.flag, strace=args.strace)
+@with_challenges
+def handle_verify(args, challenges):
+    for challenge in challenges:
+        print(f"Verifying {type(challenge).__name__}.")
+        verify_challenge(challenge, debug=args.debug, flag=args.flag, strace=args.strace)
 
 def handle_verify_module(args):
     for n,c in enumerate(c(seed=args.seed, walkthrough=args.walkthrough) for c in pwnshop.MODULE_LEVELS[args.module]):
@@ -162,7 +169,7 @@ def main():
 
     # common to all single-challenge commands
     for subparser in [ command_render, command_build, command_verify ]:
-        subparser.add_argument("challenge", help="the challenge, as ChallengeClassName or ModuleName:level_number")
+        subparser.add_argument("challenges", help="the challenges, as multiple ChallengeClassName or ModuleName:level_number", nargs="+")
 
     command_verify_module.add_argument("module", help="the module to verify")
 
