fd3dceb71039f48d7890aa5bc958267dd45b0db4
Yan <yans@yancomm.net>
2024-11-13 16:39:45 -0700

    handle timeouts during dockerized verification

--- pwnshop/challenge.py
+++ pwnshop/challenge.py
@@ -283,11 +283,12 @@ class Challenge:
 
         if self._verify_container:
             docker_cmd = "docker exec -u root -i".split()
-            for k,v in kwargs.get("env", {}).items():
+            for k,v in kwargs.pop("env", {}).items():
                 docker_cmd += [ "-e", f"{k}={v.decode() if type(v) is bytes else v}" ]
-            kwargs.pop("env", None)
             docker_cmd += [ "-w", self.work_dir ]
             docker_cmd.append(self._verify_container.name)
+            if alarm := kwargs.pop("alarm", None):
+                docker_cmd += [ "timeout", "-sALRM", str(alarm) ]
             argv = docker_cmd + argv
 
         with pwnlib.tubes.process.process(
