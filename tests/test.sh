a8a980466f10564e07f2ac1a997719b98dd485a1
Yan <yans@yancomm.net>
2024-11-16 21:40:34 -0700

    test parallel building

--- tests/test.sh
+++ tests/test.sh
@@ -28,6 +28,17 @@ LIBS=( ../example_deploy/*/*/lib/* )
 [ "${#FILES[@]}" -eq 8 ] || exit 1
 [ "${#LIBS[@]}" -eq 3 ] || exit 1
 
+rm -rf ../example_deploy/*/*/*
+pwnshop apply ../example_deploy/pwnshop.yml --mp --quiet
+SOURCES=( ../example_deploy/*/*/*.c )
+BINS=( ../example_deploy/*/*/shell )
+FILES=( ../example_deploy/*/*/* )
+LIBS=( ../example_deploy/*/*/lib/* )
+[ "${#SOURCES[@]}" -eq 1 ] || exit 1
+[ "${#BINS[@]}" -eq 6 ] || exit 1
+[ "${#FILES[@]}" -eq 8 ] || exit 1
+[ "${#LIBS[@]}" -eq 3 ] || exit 1
+
 cd /
 pwnshop -C $EXM render ShellExample
 
