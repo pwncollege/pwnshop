42ec94a08b6018ab501295cbf08cd7cc6dccb39e
Yan <yans@yancomm.net>
2025-01-27 19:30:42 -0700

    set the path. Thanks to supercoolspy for the fix

--- pwnshop/templates/base/selenium.py
+++ pwnshop/templates/base/selenium.py
@@ -21,7 +21,7 @@ os.environ["PATH"] = "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/b
 
 options = FirefoxOptions()
 options.add_argument("--headless")
-service = FirefoxService(log_path="/dev/null")
+service = FirefoxService(log_path="/dev/null", executable_path=shutils.which("geckodriver"))
 browser = webdriver.Firefox(service=service, options=options)
 atexit.register(browser.quit)
 
