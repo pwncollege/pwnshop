f50827700e3520b09a4d013bf688799ede1ca488
Yan <yans@yancomm.net>
2025-01-27 20:37:47 -0700

    actual fix

--- pwnshop/templates/base/selenium.py
+++ pwnshop/templates/base/selenium.py
@@ -1,5 +1,6 @@
 #!/opt/pwn.college/python
 
+import shutil
 import psutil
 import urllib
 import atexit
@@ -21,7 +22,7 @@ os.environ["PATH"] = "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/b
 
 options = FirefoxOptions()
 options.add_argument("--headless")
-service = FirefoxService(log_path="/dev/null", executable_path="/usr/local/bin/geckodriver")
+service = FirefoxService(log_path="/dev/null", executable_path=shutil.which("geckodriver"))
 browser = webdriver.Firefox(service=service, options=options)
 atexit.register(browser.quit)
 
